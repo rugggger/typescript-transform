2209d316612bbbcdad4353ab104c6a173bb89451
rugggger <yaron.bar@contentsquare.com>
2020-09-02 12:44:39 +0300

    added iframe solution POC

--- app.ts
+++ app.ts
@@ -1,14 +1,16 @@
 import Compiler from "./src/compiler";
-import { getPureWindow, extendDOMWithCSNativeFunctions } from "./src/getPureWindow";
+import {
+  getPureWindow,
+  extendDOMWithCSNativeFunctions,
+} from "./src/getPureWindow";
 
 let n = getPureWindow();
 extendDOMWithCSNativeFunctions();
 
-
 declare var native;
 
-  (function(){
-    var Date = native.Date;
+(function () {
+  var Date = native.Date;
 
   let source = `
   const s = "this string doesnt need to change";
@@ -17,16 +19,26 @@ declare var native;
   console.log(a.toUpperCase());
   `;
 
-   //let compiler = new Compiler(source);
-   //compiler.compileTransformer();
+  //let compiler = new Compiler(source);
+  //compiler.compileTransformer();
 
-  console.log('check array filter ', [1,2,3].filter(a=>a))
+  console.log(
+    "check array filter ",
+    [1, 2, 3].filter((a) => a)
+  );
 
-  console.log('check Node.childNodes ', document.querySelector('body').childNodes);
+  console.log(
+    "check Node.childNodes ",
+    document.querySelector("body").childNodes
+  );
 
   let s = "asdasd";
-  console.log('check Date.now ', Date.now());
-  console.log('check String.toLocaleUpperCase ', "abcde".toLocaleUpperCase());
-  console.log('check String.toLocaleUpperCase ', s.toLocaleUpperCase());
+  console.log("check Date.now ", Date.now());
+  console.log("check String.toLocaleUpperCase ", "abcde".toLocaleUpperCase());
+  console.log("check String.toLocaleUpperCase ", s.toLocaleUpperCase());
 
-  })()
+  console.log('set listeners')
+  window.addEventListener("mousemove", function (e) {
+    console.log('app.ts mousemove event',e)
+  });
+})();
